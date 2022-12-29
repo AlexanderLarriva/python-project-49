76dbeebdbe2ad4f73b4001e083c29b7898aa7476
Alexander Shangin <alarriva@yandex.ru>
2022-12-29 08:04:14 +0600

    Add new game Progression

---
+++ brain_games/games/engine_progression.py
@@ -0,0 +1,30 @@
+from random import randint
+import prompt
+import brain_games.ask
+
+
+def run_play():
+    name = brain_games.ask.action()
+    print('What number is missing in the progression?')
+    score = 0
+    while (score < 3):
+        pr_list = [None] * randint(5,12)
+        pr_list[0] = randint(0, 100)
+        step = randint(0, 10)
+        for i in range(1, len(pr_list)):
+            pr_list[i] = pr_list[i - 1] + step
+        hidden_num = randint(0, len(pr_list) - 1)
+        txt_pr_list = list(map(str, pr_list))
+        txt_pr_list[hidden_num] = '..'
+        print("Question: ", *txt_pr_list)
+        answer = int(prompt.integer('Your answer: '))
+        # print('answer = ', type(answer)) проверка типов данных
+        # print('target = ', type(pr_list[hidden_num]))
+        if (answer == pr_list[hidden_num]):
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. \
+Correct answer was '{pr_list[hidden_num]}'.")
+            break
+        score += 1
+    brain_games.ask.is_win(score, name)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import brain_games.games.engine_progression
+
+
+def main():
+    brain_games.games.engine_progression.run_play()
+
+
+if __name__ == '__main__':
+    main()
