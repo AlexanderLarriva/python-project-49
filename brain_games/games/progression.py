d96ddacc696d02c3353427a0383cf5178d31f569
Alexander Shangin <alarriva@yandex.ru>
2023-01-16 18:34:37 +0600

    Refactor file

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,6 +4,7 @@ from random import randint
 TASK = 'What number is missing in the progression?'
 MIN_LENGTH_PROGRESSION = 5
 MAX_LENFTH_PROGRESSION = 10
+RANGE_LIMIT = 1000000
 
 
 def get_result(progression):
@@ -18,13 +19,10 @@ def get_result(progression):
 
 def get_question_answer():
     '''Generates task values and a question to the player.'''
-    progression = []
     length_progression = randint(MIN_LENGTH_PROGRESSION, MAX_LENFTH_PROGRESSION)
     start = randint(0, 100)
     step = randint(1, 10)
-    stop = start + step * length_progression
-    for i in range(start, stop, step):
-        progression.append(i)
+    progression = list(range(start, RANGE_LIMIT, step)[:length_progression])
     correct_answer, progression_str = get_result(progression)
     question = f"Question: {progression_str}"
     return question, correct_answer
